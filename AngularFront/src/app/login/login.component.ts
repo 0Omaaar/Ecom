dcc6b7440ef25b6afb49e07811657895dea96673
0Omaaar <elkhotriomarpro@gmail.com>
2024-04-08 02:41:34 +0000

    login front api success

--- AngularFront/src/app/login/login.component.ts
+++ AngularFront/src/app/login/login.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  hidePassword = true;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, Validators.required],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void {
+    const username = this.loginForm.get('email').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.login(username, password).subscribe(
+      (response) => {
+        this.snackBar.open('Login Success', 'OK', { duration: 5000 });
+      },
+      (error) => {
+        this.snackBar.open('Login Failed', 'ERROR', { duration: 5000 });
+      }
+    );
+  }
 }
