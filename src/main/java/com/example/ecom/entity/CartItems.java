64be64f63e9dc539f4747054a4f541034b5b360f
0Omaaar <elkhotriomarpro@gmail.com>
2024-04-17 11:34:05 +0100

    order + cartItems entities

--- src/main/java/com/example/ecom/entity/CartItems.java
+++ src/main/java/com/example/ecom/entity/CartItems.java
@@ -2,6 +2,8 @@ package com.example.ecom.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity @Data
 public class CartItems {
@@ -14,5 +16,16 @@ public class CartItems {
     private Long quantity;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
--- src/main/java/com/example/ecom/services/auth/AuthServiceImpl.java
+++ src/main/java/com/example/ecom/services/auth/AuthServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.ecom.services.auth;
 
 import com.example.ecom.dto.SignupRequest;
 import com.example.ecom.dto.UserDto;
+import com.example.ecom.entity.Order;
 import com.example.ecom.entity.User;
+import com.example.ecom.enums.OrderStatus;
 import com.example.ecom.enums.UserRole;
+import com.example.ecom.repository.OrderRepository;
 import com.example.ecom.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +23,9 @@ public class AuthServiceImpl implements AuthService{
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     public UserDto createUser(SignupRequest signupRequest){
         User user = new User();
 
@@ -28,6 +34,14 @@ public class AuthServiceImpl implements AuthService{
         user.setPassword(new BCryptPasswordEncoder().encode(signupRequest.getPassword()));
         user.setRole(UserRole.CUSTOMER);
         User createdUser = userRepository.save(user);
+
+        Order order = new Order();
+        order.setAmount(0L);
+        order.setTotalAmount(0L);
+        order.setDiscount(0L);
+        order.setUser(createdUser);
+        order.setOrderStatus(OrderStatus.Pending);
+        orderRepository.save(order);
 
         UserDto userDto = new UserDto();
         userDto.setId(createdUser.getId());
